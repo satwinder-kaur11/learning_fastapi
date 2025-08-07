fde73e723f70cc424d4f66c075205d7fa5b16c45
Satwinder Kaur <ksatwinder865@gmail.com>
2025-08-07 12:19:01 +0530

    path query parameter added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI,Path,HTTPException
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 app = FastAPI()
 
@@ -26,4 +26,21 @@ def view_patient(patient_id:str=Path(...,description='id of the patient in DB',e
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404,detail = 'Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by:str=Query(...,Description='sort on the basis of  height,weight,or bmi'),
+                  order: str =Query('asc',Description=' sort on asc or desc order')):
+    
+    valid_fields =['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'invalid filed select from {valid_fields}')
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail='invalid order selcect between asc and  desc')
+    
+    data =load_data()
+
+    sort_data =True if  order== 'desc' else 'False'
+
+    sorted_data = sorted(data.values(),key=lambda x: x.get(sort_by,0),reverse=sort_data)
+    return sorted_data
 
